6e29a1e6e52ab25b917c2c67320ff105100de5e1
frank <frank581.fgz@gmail.com>
2025-02-05 00:29:14 +0100

    Aggiunta architettura di comunicazione 2

--- Architecture/SeparateSuggestion.py
+++ Architecture/SeparateSuggestion.py
@@ -2,14 +2,14 @@ from NPC.NPC import NPC
 import random as rand
 import numpy as np
 
-class InjectionHelper:
+class SeparateHelper:
     def __init__(self, npc : NPC):
         self.helper = npc
 
-    def inject_suggestion(self, description_of_game_state, prob = 0):
+    def get_suggestion(self, description_of_game_state, prob = 0):
         suggestion = -1
 
-        if rand == 0 or rand.random() < prob:
+        if prob == 0 or rand.random() < prob: 
             suggestion =  self.helper.get_advice(description_of_game_state)
         
         return suggestion
