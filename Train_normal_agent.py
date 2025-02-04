a80cd525917d1dc24fdea356a173e3f53c9c2597
Annalaura Miglino <miglinoannalaura@gmail.com>
2025-02-04 19:01:28 +0100

    Bug fix
    Aggiunto salvataggio corretto del modello

--- Train_normal_agent.py
+++ Train_normal_agent.py
@@ -82,4 +82,4 @@ salva_csv(reward_per_episode, "Reward", "csv_reward_DQN.csv")
 salva_csv(step_per_episode, "Steps", "csv_steps_DQN.csv")
 salva_csv(epsilon_value, "Epsilon", "csv_epsilon_DQN.csv")
 
-agent.save("./models")
+agent.save("./models/model.pth" )
