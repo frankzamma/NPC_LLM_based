489144ed0bbdc89d318042f1d0e8cc576cd171f9
Annalaura Miglino <miglinoannalaura@gmail.com>
2025-02-06 16:27:47 +0100

    Update
    Aggiornato prompt

--- NPC/Gemini_NPC.py
+++ NPC/Gemini_NPC.py
@@ -18,11 +18,24 @@ class Gemini_NPC(NPC):
         # genai.configure(api_key=self.key)
 
         self.system_message = \
-                    "You are an assitant of game player."\
-                    "You receive the state of the game you suggest a possible strategies."\
-                    "Your suggestion has two parts: description and action."\
-                    "description is a brief descritpion of strategy, max 200 words."\
-                    "action is a number beetween  0 and 8."\
+                    "You are a game asstitant for player."\ 
+                    "The battle involves a player and an enemy. Both player and enemy are characterised by:"\
+                    "HP (health point), MP (magic point), Attack points, Defence points, Available magic points and Available items. "\
+                    "For spells, the use of MP is necessary, while items have limited availability. "\
+                    "The player has 9 actions available: Attack (removes 300 HP from the enemy), which is of type Attack [0],"\
+                    "Fire (removes 600 HP from the enemy at a cost of 25 MP of the player), which is of type Magic [1],"\
+                    "Thunder (removes 700 HP from the enemy at a cost of 30 MP of the player), which is of type Magic [2], "\
+                    "Blizzard (removes 700 HP from the enemy at a cost of 35 MP of the player), which is of type Magic [3], "\
+                    "Meteor (removes 1000 HP from the enemy at a cost of 40 MP of the player), which is of type Magic [4], "\
+                    "Cure (adds 1500 HP to the player at a cost of 32 MP of the player), which is of type Magic [5], "\
+                    "Potion (adds 50 HP to the player), which is of type Item and the player has 3 [6], "\
+                    "Grenade (removes 500 HP from the enemy), which is of type Item and the player has 2 [7], "\
+                    "Elixir (restores all HP and MP points of the player), which is Item type and the player has 1 [8]."\
+                    "The number beetween [] is the id of the action."\
+                    "You receive the state of the game and you formulate a strategy for the player."\
+                    "The suggestion has 2 part: description and action."\
+                    "The description is a brief descritpion of proposed strategy, max 200 words."\
+                    "Action is the action that you suggest."\
                     
 
         
