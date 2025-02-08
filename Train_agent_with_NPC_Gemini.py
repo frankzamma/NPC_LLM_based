b10d1af772a99ef0755b869ec30e5bc705014c7a
Annalaura Miglino <miglinoannalaura@gmail.com>
2025-02-08 10:04:45 +0100

    Aggiornata classe
    Aggiornata classe per probabilità 0.3

--- Train_agent_with_NPC_Gemini.py
+++ Train_agent_with_NPC_Gemini.py
@@ -13,13 +13,14 @@ import datetime
 import re
 
 learning_rate = 0.01
-n_episodes = 1500
+n_episodes = 1200
 start_epsilon = 1.0
 epsilon_decay = start_epsilon / (n_episodes / 2)  
 final_epsilon = 0.1
 
 #Probabilità che intervenga l'NPC
-PROB=1 #NPC interviene sempre
+#PROB=1 NPC interviene sempre
+PROB = 0.3
 
 # Spells and items setup
 fire = Spell("Fire", 25, 600, "black")
@@ -52,6 +53,7 @@ enemy_wins = []
 epsilon_value = []
 success_rate = []
 consigli_accettati = []
+consigli_dati = []
 total_agent_wins = 0
 
 npc =  Gemini_NPC()
@@ -82,6 +84,7 @@ for episode in tqdm(range(n_episodes)):
     total_reward = 0
     moves = 0
     consigli_accettati_episode = 0
+    consigli_dati_episode = 0
 
     obs = np.append(obs, 0)
 
@@ -94,7 +97,11 @@ for episode in tqdm(range(n_episodes)):
             consigli_accettati_episode += 1
             print("- Decisione Agente: accettato consiglio\n\n")
         else:
-            print("- Decisione Agente: ignorato consiglio\n\n")
+            if obs[len(obs) -1] == -1:
+                print("Consiglio NPC non dato\n\n")
+            else:
+                consigli_dati_episode += 1
+                print("- Decisione Agente: ignorato consiglio\n\n")
 
         # print(f"episode:{episode}, steps:{moves} - azione selezionata")
         next_obs, reward, done, a_win, e_win, enemy_choice = env.step(action)
@@ -135,6 +142,7 @@ for episode in tqdm(range(n_episodes)):
     step_per_episode.append(moves)
     epsilon_value.append(agent.epsilon)
     consigli_accettati.append(consigli_accettati_episode)
+    consigli_dati.append(consigli_dati_episode)
     print(f"Episode: {episode + 1}, Total Reward: {total_reward}")
 
     if  episode % 50 == 0:
@@ -150,18 +158,18 @@ for episode in tqdm(range(n_episodes)):
         salva_csv(enemy_wins, "Enemy_Win", f"{dir_episode}/csv_win_enemy_Gemini.csv")
         salva_csv(success_rate, "Success_Rate", f"{dir_episode}/csv_win_success_rate_Gemini.csv")
         salva_csv(consigli_accettati, "Consigli_Accettati", f"{dir_episode}/csv_consigli_accettati_Gemini.csv")
-
+        salva_csv(consigli_dati, "Consigli_dati", f"{dir_episode}/csv_consigli_dati_Gemini.csv")
 
         agent.save(f"{dir_episode}/model_Gemini_1.pth" )
 
 
-salva_csv(reward_per_episode, "Reward", f"{dir}/csv_reward_Gemini.csv")
-salva_csv(step_per_episode, "Steps", f"{dir}/csv_steps_Gemini.csv")
-salva_csv(epsilon_value, "Epsilon", f"{dir}/csv_epsilon_Gemini.csv")
-salva_csv(agent_wins, "Agent_Win", f"{dir}/csv_win_agent_Gemini.csv")
-salva_csv(enemy_wins, "Enemy_Win", f"{dir}/csv_win_enemy_Gemini.csv")
-salva_csv(success_rate, "Success_Rate", f"{dir}/csv_win_success_rate_Gemini.csv")
-salva_csv(consigli_accettati, "Consigli_Accettati", f"{dir}/csv_consigli_accettati_Gemini.csv")
-
+salva_csv(reward_per_episode, "Reward", f"{dir}/csv_reward_Gemini_FINAL.csv")
+salva_csv(step_per_episode, "Steps", f"{dir}/csv_steps_Gemini_FINAL.csv")
+salva_csv(epsilon_value, "Epsilon", f"{dir}/csv_epsilon_Gemini_FINAL.csv")
+salva_csv(agent_wins, "Agent_Win", f"{dir}/csv_win_agent_Gemini_FINAL.csv")
+salva_csv(enemy_wins, "Enemy_Win", f"{dir}/csv_win_enemy_Gemini_FINAL.csv")
+salva_csv(success_rate, "Success_Rate", f"{dir}/csv_win_success_rate_Gemini_FINAL.csv")
+salva_csv(consigli_accettati, "Consigli_Accettati", f"{dir}/csv_consigli_accettati_Gemini_FINAL.csv")
+salva_csv(consigli_dati, "Consigli_dati", f"{dir}/csv_consigli_dati_Gemini_FINAL.csv")
 
 agent.save(f"{dir}/model_Gemini_1.pth" )
