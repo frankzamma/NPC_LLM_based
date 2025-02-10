8fbf7fd3588b596dd23066b17d3f6faf2c5940e7
frank <frank581.fgz@gmail.com>
2025-02-10 23:16:25 +0100

    Aggiornato train del agente normale

---
+++ Train_normal_agent.py
@@ -0,0 +1,116 @@
+import os
+import datetime
+from tqdm import tqdm
+from Algorithms.Agent_DQN import DQNAgent
+from Algorithms.utils import salva_csv
+from resources.game import Person
+from resources.magic import Spell
+from resources.inventory import Item
+from resources.environment import BattleEnv
+
+
+learning_rate = 0.01
+n_episodes = 1_150
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)  
+final_epsilon = 0.1
+
+
+# Spells and items setup
+fire = Spell("Fire", 25, 600, "black")
+thunder = Spell("Thunder", 30, 700, "black")
+blizzard = Spell("Blizzard", 35, 800, "black")
+meteor = Spell("Meteor", 40, 1000, "black")
+cura = Spell("Cura", 32, 1500, "white")
+
+potion = Item("Potion", "potion", "Heals 50 HP", 50)
+hielixer = Item("MegaElixer", "elixer", "Fully restores party's HP/MP", 9999)
+grenade = Item("Grenade", "attack", "Deals 500 damage", 500)
+
+player_spells = [fire, thunder, blizzard, meteor, cura]
+player_items = [{"item": potion, "quantity": 3}, {"item": grenade, "quantity": 2},
+                {"item": hielixer, "quantity": 1}]
+player1 = Person("Valos", 3260, 132, 300, 34, player_spells, player_items)
+enemy1 = Person("Magus", 4000, 701, 525, 25, [fire, cura], [])
+
+players = [player1]
+enemies = [enemy1]
+
+env = BattleEnv(players, enemies)
+
+obs = env.reset()
+
+reward_per_episode = []
+step_per_episode = []
+agent_wins = []
+enemy_wins = []
+epsilon_value = []
+success_rate = []
+total_agent_wins = 0
+
+
+
+state_dim = obs.shape[0]
+action_dim = env.action_size
+agent = DQNAgent(state_dim, action_dim, lr=0.001, gamma=0.99, epsilon=1.0, epsilon_decay=0.995, buffer_size=10000)
+
+# TRAINING
+batch_size = 32
+for episode in tqdm(range(n_episodes)):
+    obs = env.reset()
+    done = False
+
+    total_reward = 0
+    moves = 0
+
+    while not done:
+        action = agent.act(obs, True)
+        # print(f"episode:{episode}, steps:{moves} - azione selezionata")
+        next_obs, reward, done, a_win, e_win, enemy_choice = env.step(action)
+        # print(f"episode:{episode}, steps:{moves} - step eseguito")
+        agent.remember(obs, action, reward, next_obs, done)
+        # print(f"episode:{episode}, steps:{moves} - remember eseguito")
+
+        agent.replay(batch_size)
+        # print(f"episode:{episode}, steps:{moves} - replay eseguito")
+
+        obs = next_obs
+        total_reward += reward
+        moves +=1
+
+        if done:
+            print(f"Episode: {episode}/{n_episodes}, Score: {total_reward}, Moves: {moves}, Epsilon: {agent.epsilon}")
+            if a_win:
+                agent_wins.append(1)
+                enemy_wins.append(0)
+                total_agent_wins += 1
+            else:
+                agent_wins.append(0)
+                enemy_wins.append(1)
+
+            success_rate.append(total_agent_wins / (episode + 1))
+            print("Vittorie agente: ", agent_wins.count(1), " Vittorie nemico: ", enemy_wins.count(1))
+
+       
+        # print(env.describe_game_state(enemy_choice))
+
+    reward_per_episode.append(total_reward)
+    step_per_episode.append(moves)
+    epsilon_value.append(agent.epsilon)
+
+    print(f"Episode: {episode + 1}, Total Reward: {total_reward}")
+
+dir = "./Results/NormalAgent/Training/" + str(1150) + "Episodes"
+
+if not(os.path.exists(dir)):
+    os.mkdir(dir)
+
+
+salva_csv(reward_per_episode, "Reward", f"{dir}/csv_reward_normal.csv")
+salva_csv(step_per_episode, "Steps", f"{dir}/csv_steps_normal.csv")
+salva_csv(epsilon_value, "Epsilon", f"{dir}/csv_epsilon_normal.csv")
+salva_csv(agent_wins, "Agent_Win", f"{dir}/csv_win_agent_normal.csv")
+salva_csv(enemy_wins, "Enemy_Win", f"{dir}/csv_win_enemy_normal.csv")
+salva_csv(success_rate, "Success_Rate", f"{dir}/csv_win_success_rate_normal.csv")
+
+agent.save(f"{dir}/model.pth" )
